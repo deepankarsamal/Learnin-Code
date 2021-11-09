542e76401578b3856511d8e73777be39ec61d0ea
deepankarsamal <58949664+deepankarsamal@users.noreply.github.com>
2021-11-09 10:06:21 +0530

    fixed scroll added color animation

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
-
 document.getElementById('btn').addEventListener('click', function() {
-    document.getElementById('text-1').innerHTML = mssg;
+    document.getElementById('text-1').innerHTML = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, ducimus!';
+});
+
+
+window.addEventListener('scroll', function() {
+
+    var scroll = document.documentElement.scrollTop;
+    if (scroll > 1000) {
+        document.getElementById('text-2').innerHTML = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, ducimus!';
+        document.getElementById('text-2').className = "slideRight";
+    }
+});
+
+window.addEventListener('scroll', function() {
+
+    var scroll = document.documentElement.scrollTop;
+    if (scroll > 400) {
+        document.getElementById('surprise').className = "slideLeft";
+    }
+});
+
+window.addEventListener('scroll', function() {
+
+    var scroll = document.documentElement.scrollTop;
+    if (scroll > 500) {
+        document.getElementById('surprise').className = "surprise";
+        
+    }
+});
+
+/*
+document.getElementById('btn').addEventListener('click', function() {
+    document.getElementById('text-1').innerHTML = 'Hmmm. U still there huh. and u blind too. And u would definitely read the message below :)';
 });
 
 window.onscroll = function() {
     var scroll = document.documentElement.scrollTop;
-    if (scroll > 820) {
-        document.getElementById('text-2').innerHTML = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, ducimus!';
-        document.getElementById('text-2').className = "slideUp";
-    }
+    if (
+        document.documentElement.scrollTop > 500){
+        document.getElementById('text-2').innerHTML = 'Hmmm. U still there huh. and u blind too. And u would definitely read the message below :)'} 
 };
 
-var mssg = 'HehE. this site was made for you to laugh at. Hope u smile when u look at this and look back at the fabulous time we had. :)';
+window.onscroll = function() {
+    var scroll = document.documentElement.scrollTop;
+    if (scroll > 400){
+        document.getElementById('surprise').style.backgroundImage = "-webkit-linear-gradient(rgb(212, 89, 126), lightseagreen)";
+    }
+    
+};
+*/
+
+
+
+// var mmsg = HehE. this site was made for you to laugh at. Hope u smile when u look at this and look back at the fabulous time we had. :)
